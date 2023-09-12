300493437d73290ca794dd5b9d7420aa3ef4d147
차진성 <ckwlstjd92@naver.com>
2023-09-12 21:50:53 +0900

    ENH: bookListPage에 책데이터 props로 전달

--- frontend/src/pages/CategoryPage.tsx
+++ frontend/src/pages/CategoryPage.tsx
@@ -20,6 +20,12 @@ const CategoryPage = () => {
         const categoryCountResponse = await getCategoryCount();
 
         if (params.id === 'ECONOMICS_MANAGEMENT') {
+          const bookListData = await getCategoryBook({
+            categoryType: 'ECONOMICS_MANAGEMENT',
+            page: 1,
+            size: 20,
+          });
+
           categoryCountResponse.forEach(
             (el: { name: string; count: number }) => {
               if (el.name === '경제/경영') {
@@ -27,6 +33,7 @@ const CategoryPage = () => {
               }
             },
           );
+          setBookList(bookListData);
         }
       } catch (error) {
         console.error('Error fetching data', error);
@@ -42,10 +49,7 @@ const CategoryPage = () => {
   return (
     <div>
       <TitleText title={title} subTitle={subtitle} />
-      <Link to={`/category/${Category['IT/프로그래밍']}`}>IT/프로그래밍</Link>
-      <Link to={`/category/${Category.과학}`}>과학</Link>
-      <Link to={`/category/${Category['기술/공학']}`}>기술/공학</Link>
-      <BookListPage params={params} />
+      <BookListPage bookList={bookList} />
     </div>
   );
 };
--- frontend/src/pages/list/BookListPage.tsx
+++ frontend/src/pages/list/BookListPage.tsx
@@ -1,23 +1,10 @@
 import { useEffect, useState } from 'react';
 
-const BookListPage = ({ params }) => {
-  useEffect(() => {
-    if (params.id === 'TECHNOLOGY_ENGINEERING') {
-      setBookList(['으라차차', '흐하하']);
-    }
-    if (params.id === 'SCIENCE') {
-      setBookList(['과학이다', '흐하하']);
-    }
-    if (params.id === 'IT_PROGRAMMING') {
-      setBookList(['아이티', '프그']);
-    }
-  }, [params]);
-  const [bookList, setBookList] = useState([]);
-
+const BookListPage = ({ bookList }) => {
   return (
     <div>
-      <div>{params.id}</div>
-      <div>{bookList}</div>
+      <div></div>
+      <div>{bookList.currentPage}</div>
     </div>
   );
 };
