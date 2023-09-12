a4b59b39b1a655299570afef6d6b893eb346b2e3
minjamie <minjae2246@gamil.com>
2023-09-13 00:38:16 +0900

    ENH : 디테일 API (리뷰, 감상평, 스터디 리스트 조회) 추가 완료

--- backend/src/main/java/com/example/bookAPI/repository/ReviewRepository.java
+++ backend/src/main/java/com/example/bookAPI/repository/ReviewRepository.java
@@ -1,8 +1,18 @@
 package com.example.bookAPI.repository;
 
 import com.example.bookAPI.domain.Review;
+import com.example.bookAPI.dto.review.ReviewBookResponseDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
+    @Query(value = "SELECT new com.example.bookAPI.dto.review.ReviewBookResponseDto(r.content, r.member.name, r.rating, r.createDateTime) " +
+            "FROM Review r " +
+            "JOIN r.book b " +
+            "WHERE b.bookId = :bookId")
+    Page<ReviewBookResponseDto> getReviewsByBookId(@Param("bookId") Long bookId, PageRequest pageable);
 }
--- backend/src/main/java/com/example/bookAPI/service/ReviewService.java
+++ backend/src/main/java/com/example/bookAPI/service/ReviewService.java
@@ -1,8 +1,11 @@
 package com.example.bookAPI.service;
 
 import com.example.bookAPI.domain.Review;
+import com.example.bookAPI.dto.review.ReviewBookResponseDto;
 import com.example.bookAPI.repository.ReviewRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,5 +18,9 @@ public class ReviewService {
     public Review saveReview(Review review) {
         Review saveReview = reviewRepository.save(review);
         return saveReview;
+    }
+    @Transactional(readOnly = true)
+    public Page<ReviewBookResponseDto> getReviewsByBookId(Long bookId, PageRequest pageable) {
+        return reviewRepository.getReviewsByBookId(bookId, pageable);
     }
 }
