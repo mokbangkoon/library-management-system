82f57b2804252d0b74403241188231ee885be159
차진성 <ckwlstjd92@naver.com>
2023-09-19 22:49:40 +0900

    ENH: 북 리스트 5개씩 나열

--- frontend/src/pages/list/BookListPage.tsx
+++ frontend/src/pages/list/BookListPage.tsx
@@ -7,7 +7,7 @@ const BookListPage = ({ bookList }) => {
     navigate(`/book/${id}`);
   };
   return (
-    <div className="grid grid-cols-4 grid-flow-row gap-5">
+    <div className="grid grid-cols-5 grid-flow-row gap-5">
       {bookList ? (
         bookList.map((el, idx) => (
           <div
@@ -25,14 +25,16 @@ const BookListPage = ({ bookList }) => {
               {el.writer} | {el.publisher}
             </div>
             <div className="flex gap-2">
-              {/* {el.shareCount !== 0 ? <div>공유해요 {el.shareCount}</div> : null}
-              {el.findCount !== 0 ? <div>책 찾아요 {el.findCount}</div> : null} */}
-              <div className="text-white font-R leading-[1.125rem] tracking-[-0.6px] px-2 py-[0.125rem] flex items-center justify-center rounded-[0.625rem] bg-[#4C6EF5]">
-                공유해요 {el.shareCount}
-              </div>
-              <div className="text-white font-R leading-[1.125rem] tracking-[-0.6px] px-2 py-[0.125rem] flex items-center justify-center rounded-[0.625rem] bg-[#7950F2]">
-                찾아요 {el.findCount}
-              </div>
+              {el.shareCount !== 0 ? (
+                <div className="text-white font-R leading-[1.125rem] tracking-[-0.6px] px-2 py-[0.125rem] flex items-center justify-center rounded-[0.625rem] bg-[#4C6EF5]">
+                  공유해요 {el.shareCount}
+                </div>
+              ) : null}
+              {el.findCount !== 0 ? (
+                <div className="text-white font-R leading-[1.125rem] tracking-[-0.6px] px-2 py-[0.125rem] flex items-center justify-center rounded-[0.625rem] bg-[#7950F2]">
+                  찾아요 {el.findCount}
+                </div>
+              ) : null}
             </div>
           </div>
         ))
